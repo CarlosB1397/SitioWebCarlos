3f9cee16056ca8c7f81ee7910111b953440fbb4e
CarlosB1397 <carlosbg1397@hotmail.com>
2026-03-25 08:15:48 -0600

    pokemon

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const cuerpo = document.body;
-const btnAzul = document.querySelector("#btnAzul");
-const btnRojo = document.querySelector("#btnRojo");
-const btnVerde = document.querySelector("#btnVerde");
+const btnBuscar = document.getElementById("btnBuscar");
+const inputNombrePokemon = document.getElementById("pokemonName");
+const sectionInfoPokemon = document.getElementById("infoPokemon");
+const body = document.body;
+const infoPokemon = document.getElementById("infoPokemon");
+btnBuscar.addEventListener("click", () => {
+  buscarPokemon(inputNombrePokemon.value.toLowerCase());
+});
 
-if (btnAzul) {
-  btnAzul.addEventListener(
-    "click",
-    () => (cuerpo.style.backgroundColor = "#3498db"),
-  );
-  btnRojo.addEventListener(
-    "click",
-    () => (cuerpo.style.backgroundColor = "#e74c3c"),
-  );
-  btnVerde.addEventListener(
-    "click",
-    () => (cuerpo.style.backgroundColor = "#2ecc71"),
-  );
+async function buscarPokemon(nombre) {
+  sectionInfoPokemon.innerHTML = "<>Buscando Pokemon...</>";
+  try {
+    const respuesta = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${inputNombrePokemon.value.toLowerCase()}`,
+    );
+    if (!respuesta.ok) {
+      throw new Error("Pokemon no encontrado");
+    }
+    const datos = await respuesta.json();
+    mostrarInfoPokemon(datos);
+    cambiarFondo(datos.types[0].type.name);
+  } catch (error) {
+    sectionInfoPokemon.innerHTML = "<>Pokemon no encontrado</>";
+    return;
+  }
 }
 
-class MenuNavegacion extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML=`
-        <nav class="navbar">
-      <div class="logo">Ejemplo Sitio Web</div>
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="contacto.html">Contacto</a></li>
-                <li><a href="https://google.com" target="_blank">Google</a></li>
-            </ul>
-         </nav>
-    `;
-  } // fin connectedCallback
-} //fin clase
+function mostrarInfoPokemon(datos) {
+  sectionInfoPokemon.innerHTML = `
+    <div>
+        <img src="${datos.sprites.other["official-artwork"].front_default}" alt="${datos.name}" />
+        <h2>${datos.name.toUpperCase()}</h2>
+        <p>${datos.types.map((type) => type.type.name).join(", ")}</p>
+        <p>Altura: ${datos.height / 10} m</p>
+        <p>Peso: ${datos.weight / 10} kg</p>
 
-customElements.define('menu-navegacion', MenuNavegacion);
+    </div>
+    `;
+}
+
+function cambiarFondo(tipo) {
+  if (tipo === "fire") {
+    body.style.backgroundColor = "red";
+    infoPokemon.style.backgroundColor = "rgba(220, 93, 73, 0.8)";
+  } else {
+    if (tipo === "water") {
+      body.style.backgroundColor = "rgba(50, 91, 240, 0.8)";
+      infoPokemon.style.backgroundColor = "rgba(148, 167, 236, 0.8)";
+    } else {
+      if (tipo === "grass") {
+        body.style.backgroundColor = "green";
+        infoPokemon.style.backgroundColor = "rgba(141, 226, 160, 0.8)";
+
+      }else {
+        body.style.backgroundColor = "white";
+        infoPokemon.style.backgroundColor = "rgba(204, 71, 200, 0.95)";
+
+      }
+    }
+  }
+}
